ad539e23c1f67447aa4b3d0d6ac244f5a8711286
Alin Rosculet <alin_ionut.rosculet@stud.acs.upb.ro>
2021-01-23 12:41:42 +0200

    delete idea folder; add lombok

--- src/main/java/com/example/springsocial/payload/LoginRequest.java
+++ src/main/java/com/example/springsocial/payload/LoginRequest.java
@@ -1,11 +1,14 @@
 package com.example.springsocial.payload;
 
+import lombok.Data;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 /**
  * Created by rajeevkumarsingh on 02/08/17.
  */
+@Data
 public class LoginRequest {
     @NotBlank
     @Email
@@ -13,20 +16,4 @@ public class LoginRequest {
 
     @NotBlank
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
